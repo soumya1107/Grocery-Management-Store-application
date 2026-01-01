f28a8bb20dac2d7f696f3b0dcb6989f1618a442b
Soumyadeep Das <64662510+soumya1107@users.noreply.github.com>
2026-01-01 23:01:02 +0530

    Add files via upload

---
+++ UI/common.js
@@ -0,0 +1,66 @@
+// Define your api here
+var productListApiUrl = 'http://127.0.0.1:5000/getProducts';
+var uomListApiUrl = 'http://127.0.0.1:5000/getUOM';
+var productSaveApiUrl = 'http://127.0.0.1:5000/insertProduct';
+var productDeleteApiUrl = 'http://127.0.0.1:5000/deleteProduct';
+var productEditApiUrl = 'http://127.0.0.1:5000/editProduct';
+var orderListApiUrl = 'http://127.0.0.1:5000/getAllOrders';
+var orderSaveApiUrl = 'http://127.0.0.1:5000/insertOrder';
+
+// For product drop in order
+var productsApiUrl = 'https://fakestoreapi.com/products';
+
+// this function is responsible for all the transfer of data between the backend and frontend
+// depending on the type of request POST or GET it either fetches or enters data in the database
+function callApi(method, url, data) {
+    $.ajax({
+        method: method,
+        url: url,
+        data: data
+    }).done(function( msg ) {
+        window.location.reload();
+    });
+}
+
+function calculateValue() {
+    var total = 0;
+    $(".product-item").each(function( index ) {
+        var qty = parseFloat($(this).find('.product-qty').val());
+        var price = parseFloat($(this).find('#product_price').val());
+        price = price*qty;
+        $(this).find('#item_total').val(price.toFixed(2));
+        total += price;
+    });
+    $("#product_grand_total").val(total.toFixed(2));
+}
+
+function orderParser(order) {
+    return {
+        id : order.id,
+        date : order.employee_name,
+        orderNo : order.employee_name,
+        customerName : order.employee_name,
+        cost : parseInt(order.employee_salary)
+    }
+}
+
+function productParser(product) {
+    return {
+        id : product.id,
+        name : product.employee_name,
+        unit : product.employee_name,
+        price : product.employee_name
+    }
+}
+
+function productDropParser(product) {
+    return {
+        id : product.id,
+        name : product.title
+    }
+}
+
+//To enable bootstrap tooltip globally
+// $(function () {
+//     $('[data-toggle="tooltip"]').tooltip()
+// });
